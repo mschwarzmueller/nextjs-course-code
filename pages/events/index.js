db91e3453afe29a3285da57a7aa6a2d4067b5cd9
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-05 15:16:51 +0100

    all events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,12 @@
+import { getAllEvents } from '../../dummy-data';
+import EventList from '../../components/events/event-list';
+
 function AllEventsPage() {
+  const events = getAllEvents();
+
   return (
     <div>
-      <h1>All Events</h1>
+      <EventList items={events} />
     </div>
   );
 }
