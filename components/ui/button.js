051fc0d5eabc2a7e8cfc910639106493828b01b2
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-05 15:26:01 +0100

    added events search (without logic)

--- components/ui/button.js
+++ components/ui/button.js
@@ -3,10 +3,18 @@ import Link from 'next/link';
 import classes from './button.module.css';
 
 function Button(props) {
+  if (props.link) {
+    return (
+      <Link href={props.link}>
+        <a className={classes.btn}>{props.children}</a>
+      </Link>
+    );
+  }
+
   return (
-    <Link href={props.link}>
-      <a className={classes.btn}>{props.children}</a>
-    </Link>
+    <button className={classes.btn} onClick={props.onClick}>
+      {props.children}
+    </button>
   );
 }
 
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,17 @@
+import { Fragment } from 'react';
+
 import { getAllEvents } from '../../dummy-data';
 import EventList from '../../components/events/event-list';
+import EventsSearch from '../../components/events/events-search';
 
 function AllEventsPage() {
   const events = getAllEvents();
 
   return (
-    <div>
+    <Fragment>
+      <EventsSearch />
       <EventList items={events} />
-    </div>
+    </Fragment>
   );
 }
 
