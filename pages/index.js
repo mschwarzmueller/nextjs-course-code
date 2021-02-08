80c7dda5e10452b8dd25b206f458bbc0dd8e3da6
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-08 15:06:24 +0100

    added getstaticprops with file

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
+import path from 'path';
+import fs from 'fs/promises';
+
 function HomePage(props) {
+  const { products } = props;
+
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return {
+    props: {
+      products: data.products
+    },
+  };
 }
 
 export default HomePage;
