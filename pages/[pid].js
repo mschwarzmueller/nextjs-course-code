9f6bc63c2412b7f5bb5ff261c4f28d562041e137
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-08 15:41:42 +0100

    getstaticpaths

---
+++ pages/[pid].js
@@ -0,0 +1,46 @@
+import path from 'path';
+import fs from 'fs/promises';
+
+import { Fragment } from 'react';
+
+function ProductDetailPage(props) {
+  const { loadedProduct } = props;
+
+  return (
+    <Fragment>
+      <h1>{loadedProduct.title}</h1>
+      <p>{loadedProduct.description}</p>
+    </Fragment>
+  );
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+
+  const productId = params.pid;
+
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  const product = data.products.find((product) => product.id === productId);
+
+  return {
+    props: {
+      loadedProduct: product,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { pid: 'p1' } },
+      { params: { pid: 'p2' } },
+      { params: { pid: 'p3' } },
+    ],
+    fallback: false
+  };
+}
+
+export default ProductDetailPage;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
 import path from 'path';
 import fs from 'fs/promises';
 
+import Link from 'next/link';
+
 function HomePage(props) {
   const { products } = props;
 
   return (
     <ul>
       {products.map((product) => (
-        <li key={product.id}>{product.title}</li>
+        <li key={product.id}>
+          <Link href={`/${product.id}`}>{product.title}</Link>
+        </li>
       ))}
     </ul>
   );
@@ -22,9 +26,9 @@ export async function getStaticProps(context) {
   if (!data) {
     return {
       redirect: {
-        destination: '/no-data'
-      }
-    }
+        destination: '/no-data',
+      },
+    };
   }
 
   if (data.products.length === 0) {
