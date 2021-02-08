77054227dac57af627707d1254afac0cca2c7a1b
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-08 15:22:52 +0100

    more getstaticprops return options

--- pages/index.js
+++ pages/index.js
@@ -13,17 +13,29 @@ function HomePage(props) {
   );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
   console.log('(Re-)Generating...');
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/no-data'
+      }
+    }
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+
   return {
     props: {
-      products: data.products
+      products: data.products,
     },
-    revalidate: 10
+    revalidate: 10,
   };
 }
 
