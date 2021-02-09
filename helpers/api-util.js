285d6348b22b0cc275cd9c4bf7cc53f2744ff42a
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-09 16:49:48 +0100

    get server side props

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -23,3 +23,16 @@ export async function getEventById(id) {
   const allEvents = await getAllEvents();
   return allEvents.find((event) => event.id === id);
 }
+
+export async function getFilteredEvents(dateFilter) {
+  const { year, month } = dateFilter;
+
+  const allEvents = await getAllEvents();
+
+  let filteredEvents = allEvents.filter((event) => {
+    const eventDate = new Date(event.date);
+    return eventDate.getFullYear() === year && eventDate.getMonth() === month - 1;
+  });
+
+  return filteredEvents;
+}
