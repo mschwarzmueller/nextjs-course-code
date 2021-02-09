ca9eec3176a4a55fa33647c1f6db7afbcaeb749c
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-09 16:05:00 +0100

    fetching data for the homepage

---
+++ helpers/api-util.js
@@ -0,0 +1,20 @@
+export async function getAllEvents() {
+  const response = await fetch('https://nextjs-course-c81cc-default-rtdb.firebaseio.com/events.json');
+  const data = await response.json();
+
+  const events = [];
+
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key]
+    });
+  }
+
+  return events;
+}
+
+export async function getFeaturedEvents() {
+  const allEvents = await getAllEvents();
+  return allEvents.filter((event) => event.isFeatured);
+}
