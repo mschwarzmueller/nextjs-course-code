f6be5639e3c784f483277596027746a18d49298e
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-11 09:25:04 +0100

    next image

--- components/event-detail/event-logistics.js
+++ components/event-detail/event-logistics.js
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import AddressIcon from '../icons/address-icon';
 import DateIcon from '../icons/date-icon';
 import LogisticsItem from './logistics-item';
@@ -16,7 +18,7 @@ function EventLogistics(props) {
   return (
     <section className={classes.logistics}>
       <div className={classes.image}>
-        <img src={`/${image}`} alt={imageAlt} />
+        <Image src={`/${image}`} alt={imageAlt} width={400} height={400} />
       </div>
       <ul className={classes.list}>
         <LogisticsItem icon={DateIcon}>
--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import Button from '../ui/button';
 import DateIcon from '../icons/date-icon';
 import AddressIcon from '../icons/address-icon';
@@ -17,7 +19,7 @@ function EventItem(props) {
 
   return (
     <li className={classes.item}>
-      <img src={'/' + image} alt={title} />
+      <Image src={'/' + image} alt={title} width={250} height={160} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
