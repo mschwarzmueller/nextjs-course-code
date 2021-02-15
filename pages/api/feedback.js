f82dd8d2a70fb03c69047deb953e60a0cd9f6499
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-15 09:55:17 +0100

    getstaticprops and api routes

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,11 +1,11 @@
 import fs from 'fs';
 import path from 'path';
 
-function buildFeedbackPath() {
+export function buildFeedbackPath() {
   return path.join(process.cwd(), 'data', 'feedback.json');
 }
 
-function extractFeedback(filePath) {
+export function extractFeedback(filePath) {
   const fileData = fs.readFileSync(filePath);
   const data = JSON.parse(fileData);
   return data;
---
+++ pages/feedback/index.js
@@ -0,0 +1,23 @@
+import { buildFeedbackPath, extractFeedback } from '../api/feedback';
+
+function FeedbackPage(props) {
+  return (
+    <ul>
+      {props.feedbackItems.map((item) => (
+        <li key={item.id}>{item.text}</li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getStaticProps() {
+  const filePath = buildFeedbackPath();
+  const data = extractFeedback(filePath);
+  return {
+    props: {
+      feedbackItems: data,
+    },
+  };
+}
+
+export default FeedbackPage;
