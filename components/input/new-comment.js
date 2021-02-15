26a6b230c965996f99bd616a2e8617d295a8efad
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-15 16:06:02 +0100

    updated starting state

--- components/input/new-comment.js
+++ components/input/new-comment.js
@@ -40,16 +40,16 @@ function NewComment(props) {
       <div className={classes.row}>
         <div className={classes.control}>
           <label htmlFor='email'>Your email</label>
-          <input type='email' id='email' />
+          <input type='email' id='email' ref={emailInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='name'>Your name</label>
-          <input type='text' id='name' />
+          <input type='text' id='name' ref={nameInputRef} />
         </div>
       </div>
       <div className={classes.control}>
         <label htmlFor='comment'>Your comment</label>
-        <textarea id='comment' rows='5'></textarea>
+        <textarea id='comment' rows='5' ref={commentInputRef}></textarea>
       </div>
       {isInvalid && <p>Please enter a valid email address and comment!</p>}
       <button>Submit</button>
