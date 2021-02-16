3f06a6d76f69d9ac4ab8e1ee03367c2b9c822f28
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-16 13:13:07 +0100

    showing and hiding the notification for newsletter signup

--- components/ui/notification.js
+++ components/ui/notification.js
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
+
 import classes from './notification.module.css';
+import NotificationContext from '../../store/notification-context';
 
 function Notification(props) {
+  const notificationCtx = useContext(NotificationContext);
+
   const { title, message, status } = props;
 
   let statusClasses = '';
@@ -20,7 +25,7 @@ function Notification(props) {
   const activeClasses = `${classes.notification} ${statusClasses}`;
 
   return (
-    <div className={activeClasses}>
+    <div className={activeClasses} onClick={notificationCtx.hideNotification}>
       <h2>{title}</h2>
       <p>{message}</p>
     </div>
--- store/notification-context.js
+++ store/notification-context.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 const NotificationContext = createContext({
   notification: null, // { title, message, status }
@@ -8,6 +8,22 @@ const NotificationContext = createContext({
 
 export function NotificationContextProvider(props) {
   const [activeNotification, setActiveNotification] = useState();
+
+  useEffect(() => {
+    if (
+      activeNotification &&
+      (activeNotification.status === 'success' ||
+        activeNotification.status === 'error')
+    ) {
+      const timer = setTimeout(() => {
+        setActiveNotification(null);
+      }, 3000);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [activeNotification]);
 
   function showNotificationHandler(notificationData) {
     setActiveNotification(notificationData);
