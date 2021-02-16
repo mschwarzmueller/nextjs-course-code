78edfb0adf6b8f55d1a75e99f2e8fd245e5b6543
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-16 08:03:27 +0100

    newsletter mongodb storage

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
 
@@ -7,7 +9,15 @@ function handler(req, res) {
       return;
     }
 
-    console.log(userEmail);
+    const client = await MongoClient.connect(
+      'mongodb+srv://maximilian:8ZO3ycZqJ23kWBQx@cluster0.ntrwp.mongodb.net/newsletter?retryWrites=true&w=majority'
+    );
+    const db = client.db();
+
+    await db.collection('emails').insertOne({ email: userEmail });
+
+    client.close();
+  
     res.status(201).json({ message: 'Signed up!' });
   }
 }
