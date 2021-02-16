26cce138487f79f6b7dd08b020bbab6cdd618229
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-16 11:32:30 +0100

    added notification component - starting setup

---
+++ components/ui/notification.js
@@ -0,0 +1,30 @@
+import classes from './notification.module.css';
+
+function Notification(props) {
+  const { title, message, status } = props;
+
+  let statusClasses = '';
+
+  if (status === 'success') {
+    statusClasses = classes.success;
+  }
+
+  if (status === 'error') {
+    statusClasses = classes.error;
+  }
+
+  if (status === 'pending') {
+    statusClasses = classes.pending;
+  }
+
+  const activeClasses = `${classes.notification} ${statusClasses}`;
+
+  return (
+    <div className={activeClasses}>
+      <h2>{title}</h2>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default Notification;
