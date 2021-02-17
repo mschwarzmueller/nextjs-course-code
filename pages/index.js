8320dfb41115fdbf4980318d6b6e5f114f0982d0
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-17 07:54:59 +0100

    created empty hero and featured-posts cmp with styling

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
-function HomePage() {}
+import { Fragment } from 'react';
+
+function HomePage() {
+  return (
+    <Fragment>
+      <Hero />
+      <FeaturedPosts />
+    </Fragment>
+  );
+}
 
 export default HomePage;
