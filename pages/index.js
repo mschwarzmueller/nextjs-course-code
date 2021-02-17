8abef7f8e0d826595f385aa9f51943a726d2fa6e
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-17 08:04:18 +0100

    added hero cmp with image

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import { Fragment } from 'react';
 
+import Hero from '../components/home-page/hero';
+
 function HomePage() {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts />
     </Fragment>
   );
 }
