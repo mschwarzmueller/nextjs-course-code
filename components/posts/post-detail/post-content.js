a97a6786b488d2814e5678ab3f2139239b31a6bd
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-17 10:29:13 +0100

    rendering a custom next image from markdown content

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,4 +1,5 @@
 import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
 
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
@@ -8,10 +9,43 @@ function PostContent(props) {
 
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+  const customRenderers = {
+    // image(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    paragraph(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].type === 'image') {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.url}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown renderers={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
--- posts/getting-started-with-nextjs.md
+++ posts/getting-started-with-nextjs.md
@@ -6,6 +6,28 @@ excerpt: NextJS is a the React framework for production - it makes building full
 isFeatured: true
 ---
 
-# This is a title
+NextJS is a **framework for ReactJS**.
 
-This is some regular text with a [link](https://google.com)
+Wait a second ... a "framework" for React? Isn't React itself already a framework for JavaScript?
+
+Well ... first of all, React is a "library" for JavaScript. That seems to be important for some people.
+
+Not for me, but still, there is a valid point: React already is a framework / library for JavaScript. So it's already an extra layer on top of JS.
+
+## Why would we then need NextJS?
+
+Because NextJS makes building React apps easier - especially React apps that should have server-side rendering (though it does way more than just take care of that).
+
+In this article, we'll dive into the core concepts and features NextJS has to offer:
+
+- File-based Routing
+- Built-in Page Pre-rendering
+- Rich Data Fetching Capabilities
+- Image Optimization
+- Much More
+
+## File-based Routing
+
+![Create routes via your file + folder structure](nextjs-file-based-routing.png)
+
+... More content ...
