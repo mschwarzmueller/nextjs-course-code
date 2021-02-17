27a57af7683c4eb549ec2bb9e0cf6b1c6a7193f4
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-17 10:36:54 +0100

    rendering code snippets

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,5 +1,7 @@
 import ReactMarkdown from 'react-markdown';
 import Image from 'next/image';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
@@ -39,6 +41,17 @@ function PostContent(props) {
       }
 
       return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { language, value } = code;
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={value}
+        />
+      );
     },
   };
 
