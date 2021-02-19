e5acca875cba2d17867f9bd91d3ae5de538b8263
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-19 09:42:04 +0100

    added signup api route

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,36 @@
+import { hashPassword } from '../../../lib/auth';
+import { connectToDatabase } from '../../../lib/db';
+
+async function handler(req, res) {
+  const data = req.body;
+
+  const { email, password } = data;
+
+  if (
+    !email ||
+    !email.includes('@') ||
+    !password ||
+    password.trim().length < 7
+  ) {
+    res.status(422).json({
+      message:
+        'Invalid input - password should also be at least 7 characters long.',
+    });
+    return;
+  }
+
+  const client = await connectToDatabase();
+
+  const db = client.db();
+
+  const hashedPassword = hashPassword(password);
+
+  const result = await db.collection('users').insertOne({
+    email: email,
+    password: hashedPassword,
+  });
+
+  res.status(201).json({ message: 'Created user!' });
+}
+
+export default handler;
