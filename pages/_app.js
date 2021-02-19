7785a79a90191e81fde54d7d9f1f9b860812edda
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-19 13:43:08 +0100

    provider wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
+import { Provider } from 'next-auth/client';
+
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider session={pageProps.session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
 
