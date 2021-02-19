1652781a7e9e9efda3265f23abec9f0395543684
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-19 13:16:31 +0100

    route protection with getsession

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,8 +1,25 @@
+import { getSession } from 'next-auth/client';
+import { useEffect, useState } from 'react';
+
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
 
 function UserProfile() {
-  // Redirect away if NOT auth
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if (!session) {
+        window.location.href = '/auth';
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, []);
+
+  if (isLoading) {
+    return <p className={classes.profile}>Loading...</p>;
+  }
 
   return (
     <section className={classes.profile}>
