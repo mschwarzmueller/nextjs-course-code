0754cfc6b4ad69c73f5a7949c9734e67c6fe1148
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-19 13:25:50 +0100

    route protection with getserversideprops

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,25 +1,22 @@
-import { getSession } from 'next-auth/client';
-import { useEffect, useState } from 'react';
-
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
 
 function UserProfile() {
-  const [isLoading, setIsLoading] = useState(true);
+  // const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    getSession().then((session) => {
-      if (!session) {
-        window.location.href = '/auth';
-      } else {
-        setIsLoading(false);
-      }
-    });
-  }, []);
+  // useEffect(() => {
+  //   getSession().then((session) => {
+  //     if (!session) {
+  //       window.location.href = '/auth';
+  //     } else {
+  //       setIsLoading(false);
+  //     }
+  //   });
+  // }, []);
 
-  if (isLoading) {
-    return <p className={classes.profile}>Loading...</p>;
-  }
+  // if (isLoading) {
+  //   return <p className={classes.profile}>Loading...</p>;
+  // }
 
   return (
     <section className={classes.profile}>
--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,26 @@
+import { getSession } from 'next-auth/client';
+
 import UserProfile from '../components/profile/user-profile';
 
 function ProfilePage() {
   return <UserProfile />;
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
 }
 
 export default ProfilePage;
