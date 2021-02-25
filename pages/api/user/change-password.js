1ccc99fea0c2998f1a863c076995d6b582b991d4
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-25 08:40:13 +0100

    added change password api route

---
+++ pages/api/user/change-password.js
@@ -0,0 +1,55 @@
+import { getSession } from 'next-auth/client';
+
+import { hashPassword, verifyPassword } from '../../../lib/auth';
+import { connectToDatabase } from '../../../lib/db';
+
+async function handler(req, res) {
+  if (req.method !== 'PATCH') {
+    return;
+  }
+
+  const session = await getSession({ req: req });
+
+  if (!session) {
+    res.status(401).json({ message: 'Not authenticated!' });
+    return;
+  }
+
+  const userEmail = session.user.email;
+  const oldPassword = req.body.oldPassword;
+  const newPassword = req.body.newPassword;
+
+  const client = await connectToDatabase();
+
+  const usersCollection = client.db().collection('users');
+
+  const user = await usersCollection.findOne({ email: userEmail });
+
+  if (!user) {
+    res.status(404).json({ message: 'User not found.' });
+    client.close();
+    return;
+  }
+
+  const currentPassword = user.password;
+
+  const passwordsAreEqual = await verifyPassword(oldPassword, currentPassword);
+
+  if (!passwordsAreEqual) {
+    res.status(403).json({ message: 'Invalid password.' });
+    client.close();
+    return;
+  }
+
+  const hashedPassword = await hashPassword(newPassword);
+
+  const result = await usersCollection.updateOne(
+    { email: userEmail },
+    { $set: { password: hashedPassword } }
+  );
+
+  client.close();
+  res.status(200).json({ message: 'Password updated!' });
+}
+
+export default handler;
