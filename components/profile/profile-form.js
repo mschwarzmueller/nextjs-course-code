581ddc543cf7a246e10d5771d54998327da8fcc3
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-25 09:43:21 +0100

    sending a request to protected api route

--- components/profile/profile-form.js
+++ components/profile/profile-form.js
@@ -1,15 +1,34 @@
+import { useRef } from 'react';
+
 import classes from './profile-form.module.css';
 
-function ProfileForm() {
+function ProfileForm(props) {
+  const oldPasswordRef = useRef();
+  const newPasswordRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const enteredOldPassword = oldPasswordRef.current.value;
+    const enteredNewPassword = newPasswordRef.current.value;
+
+    // optional: Add validation
+
+    props.onChangePassword({
+      oldPassword: enteredOldPassword,
+      newPassword: enteredNewPassword
+    });
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' ref={newPasswordRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor='old-password'>Old Password</label>
-        <input type='password' id='old-password' />
+        <input type='password' id='old-password' ref={oldPasswordRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -18,10 +18,24 @@ function UserProfile() {
   //   return <p className={classes.profile}>Loading...</p>;
   // }
 
+  async function changePasswordHandler(passwordData) {
+    const response = await fetch('/api/user/change-password', {
+      method: 'PATCH',
+      body: JSON.stringify(passwordData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+  }
+
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm />
+      <ProfileForm onChangePassword={changePasswordHandler} />
     </section>
   );
 }
