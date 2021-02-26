5b39367f8bcf265441d8a1414a5fac1263006121
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-26 15:28:59 +0100

    introducing state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import Todo from './components/Todo';
-import Modal from './components/Modal';
-import Backdrop from './components/Backdrop';
 
 function App() {
   return (
@@ -9,8 +7,6 @@ function App() {
       <Todo text='Learn React' />
       <Todo text='Master React' />
       <Todo text='Explore the full React course' />
-      <Modal />
-      <Backdrop />
     </div>
   );
 }
