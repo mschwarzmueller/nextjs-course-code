7bed91c7bf6f7c10e000c347a7cd0d9c682e8598
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-26 15:34:15 +0100

    added a form

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,12 @@
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
+
 function NewMeetupPage() {
-  return <div>New Meetup Page</div>;
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm />
+    </section>
+  );
 }
 
 export default NewMeetupPage;
