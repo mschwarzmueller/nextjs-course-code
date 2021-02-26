8813658c1bfbe80f847bac9bdfb3874f908aea14
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-26 15:28:24 +0100

    adding more components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Todo from './components/Todo';
+import Modal from './components/Modal';
+import Backdrop from './components/Backdrop';
 
 function App() {
   return (
@@ -7,6 +9,8 @@ function App() {
       <Todo text='Learn React' />
       <Todo text='Master React' />
       <Todo text='Explore the full React course' />
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,13 @@
 function Todo(props) {
+  function deleteHandler() {
+    
+  }
+
   return (
     <div className='card'>
       <h2>{props.text}</h2>
       <div className='actions'>
-        <button className='btn'>Delete</button>
+        <button className='btn' onClick={deleteHandler}>Delete</button>
       </div>
     </div>
   );
