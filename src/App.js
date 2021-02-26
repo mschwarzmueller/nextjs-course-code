dd06d0baecf381f01d34513e347120e4aa1ae1f6
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-26 15:27:06 +0100

    props and dynamic content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
+import Todo from './components/Todo';
+
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <div className='card'>
-        <h2>TITLE</h2>
-        <div className='actions'>
-          <button className='btn'>Delete</button>
-        </div>
-      </div>
+      <Todo text='Learn React' />
+      <Todo text='Master React' />
+      <Todo text='Explore the full React course' />
     </div>
   );
 }
