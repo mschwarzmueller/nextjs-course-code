ce107407c447f4a9af50cd14818fcb31791b0e02
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-26 15:01:07 +0100

    added notification component

---
+++ components/notification/notification.js
@@ -0,0 +1,35 @@
+import { useContext } from 'react';
+
+import classes from './notification.module.css';
+import NotificationContext from '../../store/notification-context';
+
+function Notification(props) {
+  const notificationCtx = useContext(NotificationContext);
+
+  const { title, message, status } = props;
+
+  let statusClasses = '';
+
+  if (status === 'success') {
+    statusClasses = classes.success;
+  }
+
+  if (status === 'error') {
+    statusClasses = classes.error;
+  }
+
+  if (status === 'pending') {
+    statusClasses = classes.pending;
+  }
+
+  const activeClasses = `${classes.notification} ${statusClasses}`;
+
+  return (
+    <div className={activeClasses} onClick={notificationCtx.hideNotification}>
+      <h2>{title}</h2>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default Notification;
