254229e424ee1b6852c3b0ac44f0428fe8b0c239
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-02-26 15:35:24 +0100

    navigating programmatically

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,10 @@
+import { useHistory } from 'react-router-dom';
+
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
+  const history = useHistory();
+
   function addMeetupHandler(meetupData) {
     fetch(
       'https://react-getting-started-48dec-default-rtdb.firebaseio.com/meetups.json',
@@ -8,10 +12,12 @@ function NewMeetupPage() {
         method: 'POST',
         body: JSON.stringify(meetupData),
         headers: {
-          'Content-Type': 'application/json'
-        }
+          'Content-Type': 'application/json',
+        },
       }
-    );
+    ).then(() => {
+      history.replace('/');
+    });
   }
 
   return (
