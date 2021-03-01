ea6b90b71907f7acc78accbc0b11aab9f1ef71ce
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-03-01 13:44:24 +0100

    meetup detail page

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,7 +1,15 @@
+import { useRouter } from 'next/router';
+
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
 
 function MeetupItem(props) {
+  const router = useRouter();
+
+  function showDetailsHandler() {
+    router.push('/' + props.id);
+  }
+
   return (
     <li className={classes.item}>
       <Card>
@@ -13,7 +21,7 @@ function MeetupItem(props) {
           <address>{props.address}</address>
         </div>
         <div className={classes.actions}>
-          <button>Show Details</button>
+          <button onClick={showDetailsHandler}>Show Details</button>
         </div>
       </Card>
     </li>
