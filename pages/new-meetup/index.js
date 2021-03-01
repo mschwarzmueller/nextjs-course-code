5ea916febefe835a6200f24937a73136d544d7f9
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-03-01 14:59:48 +0100

    sending a request to the api route

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,9 +1,25 @@
 // our-domain.com/new-meetup
+import { useRouter } from 'next/router';
+
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
-  function addMeetupHandler(enteredMeetupData) {
-    console.log(enteredMeetupData);
+  const router = useRouter();
+
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.push('/');
   }
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />
