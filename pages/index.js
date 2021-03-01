4ca29600944826a25e5e71c33a10fb5cb6e14b34
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-03-01 14:01:53 +0100

    added getstaticprops

--- pages/index.js
+++ pages/index.js
@@ -19,8 +19,17 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
+
+export async function getStaticProps() {
+  // fetch data from an API
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS
+    }
+  }; 
 }
 
 export default HomePage;
