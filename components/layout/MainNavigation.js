01a09278840b303de1117413ff7222a0b4fbf758
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2021-03-01 11:34:44 +0100

    adding a layout with _app.js

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import classes from './MainNavigation.module.css';
 
 function MainNavigation() {
@@ -8,10 +10,10 @@ function MainNavigation() {
       <nav>
         <ul>
           <li>
-            <Link to='/'>All Meetups</Link>
+            <Link href='/'>All Meetups</Link>
           </li>
           <li>
-            <Link to='/new-meetup'>Add New Meetup</Link>
+            <Link href='/new-meetup'>Add New Meetup</Link>
           </li>
         </ul>
       </nav>
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import Layout from '../components/layout/Layout';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
