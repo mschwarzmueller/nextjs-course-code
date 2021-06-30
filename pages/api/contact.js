2b71b32bc456972f03332785bee7e59c4b3c54ae
Sathishkumar Thangavel <19513411+SathishkumarThangavel@users.noreply.github.com>
2021-06-30 20:31:13 +0530

    Update contact.js

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -24,7 +24,7 @@ async function handler(req, res) {
 
     let client;
 
-    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.ntrwp.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
+    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.06lcn.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
 
     try {
       client = await MongoClient.connect(connectionString);
