0f6edc3ab5daf1ae9667306cb6ed4322625625f1
amir-mln <devcareer.me@gmail.com>
2022-01-14 15:10:40 +0330

    initial-commit

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # Next.js Blog
 This is the main fullstack blog application of [Maximilian Schartwzmuller's course](https://www.udemy.com/course/nextjs-react-the-complete-guide/), covered in section 11 `Complete App Example Build a Full Blog A to Z`.
-
-i forked the project in its initial empty state and all the commits previous to [ab7af51](https://github.com/amir-mln/nextjs-blog/commit/ab7af51b18c977369e4a27e03fdf852df8aa1178) do not belong to me.
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,7 @@
-import Head from 'next/head';
-
-import '../styles/globals.css';
-import Layout from '../components/layout/layout';
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
-  );
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
