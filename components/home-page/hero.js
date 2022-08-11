9af839e05198dcd4acfb8d2185f2288ce443d496
Clint <okaclint@gmail.com>
2022-08-11 11:17:09 -1000

    post layout

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 function Hero() {
   return (
     <section className="bg-gray-800">
-      <div className='p-7 flex justify-center'>
+      <div className='p-7 flex justify-center class'>
         <Image
           src="/images/site/g1.jpg"
           alt="An image showing a rainbow gradient."
