2564f8774c701fd90e0f1a2ec37940c9fa5e0c73
Clint <okaclint@gmail.com>
2022-08-12 10:22:15 -1000

    format featrue posts

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -5,7 +5,7 @@ function FeaturedPosts(props) {
   return (
     <>
     <Head>
-      <title>Mije</title>
+      <title>MIJE</title>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@700&display=swap" rel="stylesheet" />
