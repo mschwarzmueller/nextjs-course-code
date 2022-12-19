ed7f03e2ccbc2db2c84073632deec5a996f6edd0
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2022-12-19 14:54:06 +0100

    updated versions

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -15,7 +15,8 @@ function FilteredEventsPage(props) {
   const filterData = router.query.slug;
 
   const { data, error } = useSWR(
-    'https://nextjs-course-c81cc-default-rtdb.firebaseio.com/events.json'
+    'https://nextjs-course-c81cc-default-rtdb.firebaseio.com/events.json',
+    (url) => fetch(url).then(res => res.json())
   );
 
   useEffect(() => {
