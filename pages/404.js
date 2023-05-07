62958b7c915b2c9563cd6dfb0baf1b2b3b9a1d4c
viethungha0610 <viethungha0610@gmail.com>
2023-05-07 22:35:22 +0100

    finished codes for file based routing section

---
+++ pages/404.js
@@ -0,0 +1,7 @@
+function NotFoundPage() {
+  return <div>
+    <h1>Page not found!</h1>
+  </div>
+};
+
+export default NotFoundPage;
--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -5,10 +5,19 @@ function ClientProjectsPage() {
   const router = useRouter()
 
   console.log(router.query)
+
+  function loadProjectHandler() {
+    // load data ...
+    router.push({
+      pathname: '/clients/[id]/[clientprojectid]',
+      query: {id: 'max', clientprojectid: 'projecta'},
+    })
+  }
   
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   )
 }
