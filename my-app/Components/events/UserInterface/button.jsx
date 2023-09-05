ad91b8e39dc346ba683ee929442fbe0b68389546
OlgaMaseng <masengolga@gmail.com>
2023-09-05 15:32:10 +0200

    Creating Button

--- my-app/Components/events/UserInterface/button.jsx
+++ my-app/Components/events/UserInterface/button.jsx
@@ -4,10 +4,10 @@ import classes from './button.module.css'
 
 function Button(props) {
   return (
-    <Link href={props.link}>
-    <a className={classes.btn}>{props.children}</a>
+    <Link className={classes.btn}href={props.link}>
+    {props.children}
     </Link>
-  )
+  );
 }
 
 export default Button
