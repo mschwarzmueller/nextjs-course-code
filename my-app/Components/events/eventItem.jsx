f2edb221a13858beee43a0339a171376d698d135
OlgaMaseng <masengolga@gmail.com>
2023-09-05 16:01:09 +0200

    downloaded zip file from his repo and pasted it on my app

--- my-app/Components/events/eventItem.jsx
+++ my-app/Components/events/eventItem.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import link from 'next/link'
 import classes from './eventItem.module.css'
 import Button from './UserInterface/button';
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
 
 function EventItem(props) {
 
@@ -23,14 +24,18 @@ function EventItem(props) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className= {classes.date}>
+          <DateIcon/>
             <time>{humanReadableDate}</time>
           </div>
           <div className= {classes.address}>
+          <AddressIcon/>
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-         <Button link={exploreLink}>Explore Events</Button>
+         <Button link={exploreLink}><span>Explore Events</span>
+         <span className={classes.icon}><ArrowRightIcon/></span>
+         </Button>
         </div>
       </div>
     </li>
--- my-app/pages/events/[eventId].jsx
+++ my-app/pages/events/[eventId].jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import{useRouter} from 'next/router'
+import { getEventById } from '@/dummy-data';
 
 function EventsDetailPage() {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if(!event) {
+    return <p>No event Found!</p>
+  }
+  
   return (
     <div>
       <h1>Event Detail</h1>
