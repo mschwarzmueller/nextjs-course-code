3ddcfb6687d649cdb94fb367bfc6d896ef1315f1
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2023-09-28 08:28:42 +0200

    updated starting code

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -5,18 +5,16 @@ import classes from './main-navigation.module.css';
 function MainNavigation() {
   return (
     <header className={classes.header}>
-      <Link href='/'>
-        <a>
-          <div className={classes.logo}>Next Auth</div>
-        </a>
+      <Link href="/">
+        <div className={classes.logo}>Next Auth</div>
       </Link>
       <nav>
         <ul>
           <li>
-            <Link href='/auth'>Login</Link>
+            <Link href="/auth">Login</Link>
           </li>
           <li>
-            <Link href='/profile'>Profile</Link>
+            <Link href="/profile">Profile</Link>
           </li>
           <li>
             <button>Logout</button>
